ba79801be3d0b84ab8a25b7cc1dda859d97f4679
Atlaoui Tarik <atlaouitarik@.com>
2020-03-06 15:55:25 +0100

    Proto

--- SRCS/compileProto.sh
+++ SRCS/compileProto.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-
-protoc
---proto_path=/home/adrien/SAR1_Assignments/SRCS/TME/SRCS-TP/src/TME5/messages.proto
---proto_path=/home/adrien/SAR1_Assignments/SRCS/TME/SRCS-TP/src/TME5/messages.proto
---plugin=/home/adrien/Software/grpc-java/jars
---grpc-java_out=/home/adrien/SAR1_Assignments/SRCS/
+fileproto=/home/adrien/SAR1_Assignments/SRCS/TME/SRCS-TP/src/TME5/messages.proto
+protoc --proto_path=$fileproto --plugin=/home/adrien/Software/grpc-java/jars
+--grpc-java_out=/home/adrien/SAR1_Assignments/SRCS
 --java_out=/home/adrien/SAR1_Assignments/SRCS/TME/SRCS-TP/src/TME5
 messages.proto
