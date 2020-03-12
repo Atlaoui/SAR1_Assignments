05aa7f8ec6fb310dfdb49a029c1609e2eb21e814
Atlaoui Tarik <atlaouitarik@.com>
2020-03-12 17:54:53 +0100

    petit modif

--- AR/EXAM_2010/bfs.c
+++ AR/EXAM_2010/bfs.c
@@ -1,9 +1,10 @@
 #include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
-
+//git clone https://github.com/ScumPetard/Operator-Mono.git
 #define TAGINIT 0
 #define TAGMSG 1
+#define TAGUP 2
 #define NB_SITE 9
 
 int voisins[3];
@@ -22,21 +23,27 @@ void simulateur(void) {
 void Init_parcours(int myRank){
      //initialisation
     MPI_Status status;
-    int i;
+    int i,nul;
     
     MPI_Recv(&voisins, 3, MPI_INT, 0, TAGINIT, MPI_COMM_WORLD, &status);
     //fin d'initialiation
     printf("%d ",myRank);
-    for(i=0;i<2;i++)
+    for(i=0;i<2;i++){
      MPI_Send("Alo",4, MPI_CHAR, i, TAGMSG, MPI_COMM_WORLD);
+     MPI_Recv(&nul, 1, MPI_INT, i, TAGUP, MPI_COMM_WORLD, &status);
+    }
 
 }
 void Recpction_msg(int myrank, char * msg){
-    int i;
+    int i,nul;
+    MPI_Status status;
     printf("%d %s",myrank,msg);
     for(i=0;i<2;i++)
-        if(i!=-1)
+        if(i!=-1){
             MPI_Send("Ola",4, MPI_CHAR, i, TAGMSG, MPI_COMM_WORLD);
+            MPI_Recv(&nul, 1, MPI_INT, i, TAGUP, MPI_COMM_WORLD, &status);
+        }
+    MPI_Send(&nul,1,MPI_INT,voisins[2],TAGUP,MPI_COMM_WORLD);
 }
 
 /******************************************************************************/
@@ -54,14 +61,19 @@ int main (int argc, char* argv[]) {
     MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
 
     if (myRank == 0) {
+        printf("%d: va lancer le simulateur\n",myRank);
         simulateur();
-    } else if(myRank==1) {
+    } 
+    
+    if(myRank==1) {
+        printf("%d: va init le parcour\n",myRank);
         Init_parcours(myRank);
     }
     else{
         MPI_Status status;
         int i;
         char *msg;
+        printf("%d: recois sont init\n",myRank);
         MPI_Recv(&voisins, 3, MPI_INT, 0, TAGINIT, MPI_COMM_WORLD, &status);
         //recp du message
         MPI_Recv(msg, 4, MPI_CHAR, MPI_ANY_SOURCE, TAGMSG, MPI_COMM_WORLD, &status);
